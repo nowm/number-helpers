5bb11538f8722d2084ffddbcd9cf596cef8be33a
nowm <nowm@users.noreply.github.com>
2023-12-09 19:19:36 +0300

    Add functions

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # number-helpers
 Few simple helpers for numbers.
+
+## Installation
+
+```shell
+npm install number-helpers
+```
+
+## Functions
+
+### round
+
+Rounding means replacing a number with an approximate value that has a shorter, simpler, or more explicit representation. For example, replacing $23.4476 with $23.45. (From Wikipedia.)
+
+__round(number, decimals = 0)__
+
+```javascript
+import {round} from 'number-helpers';
+
+round(123.4567, 3); // 123.456
+round(123.4567, 2); // 123.45
+round(123.4567, 1); // 123.4
+round(123.4567, 0); // 123
+```
+
+### numberFormat
+
+Formats a number with manually set thousands/decimal separators. does not affect number of decimals when correspond parameter is not a number
+
+__numberFormat(number, decimals = null, decimalSeparator = '.', thousandsSeparator = ',')__
+
+```javascript
+import {numberFormat} from 'number-helpers';
+
+numberFormat(123456.789, 2, '.', ',');    // 123,456.78
+numberFormat(123456.789, 0, '.', ',');    // 123,456
+numberFormat(123456.789, null);           // 123,456.789
+```
+
+### randomNumber
+
+Generates a random number between `min` and `max`.
+
+__randomNumber(min, max)__
+
+```javascript
+import {randomNumber} from 'number-helpers';
+
+randomNumber(10, 20); // 11
+```
---
+++ index.js
@@ -0,0 +1,101 @@
+/**
+ * Checks if a variable is a finite number.
+ *
+ * NaN and Infinity values considered to be non-numbers
+ *
+ * @param {*} variable
+ *
+ * @return {boolean}
+ *
+ * @internal
+ */
+function isNumber(variable) {
+    if (typeof variable === 'bigint') {
+        return true;
+    }
+
+    if (typeof variable !== 'number') {
+        return false;
+    }
+
+    let result;
+
+    try {
+        result = isFinite(variable);
+    } catch (error) {
+        result = false;
+    }
+
+    return result;
+}
+
+/**
+ * @param {number} number
+ * @param {number} decimals
+ * @return {number}
+ */
+function round(number, decimals = 0) {
+    if (!isNumber(number)) {
+        return number;
+    }
+
+    const newDecimals = isNumber(decimals) && decimals >= 0 ? Math.round(decimals) : 0;
+    if (!newDecimals) {
+        return Math.round(number);
+    }
+
+    const power = 10 ** newDecimals;
+
+    return Math.round((number + Number.EPSILON) * power) / power;
+}
+
+/**
+ * @param {number} number
+ * @param {number|null} decimals
+ * @param {string} decimalSeparator
+ * @param {string} thousandsSeparator
+ */
+function numberFormat(number, decimals = null, decimalSeparator = '.', thousandsSeparator = ',') {
+    if (!isNumber(number)) {
+        return number;
+    }
+
+    let [integer, fraction] = number.toString().split('.');
+
+    let newDecimals = decimals;
+    if (!isNumber(newDecimals)) {
+        newDecimals = fraction ? fraction.length : 0;
+    }
+
+    fraction = fraction.substring(0, newDecimals);
+
+    integer = integer.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, thousandsSeparator);
+
+    if (!fraction) {
+        return integer;
+    }
+
+    return [integer, fraction].join(decimalSeparator);
+}
+
+/**
+ * @param {number} min
+ * @param {number} max
+ * @return {number}
+ */
+function randomNumber(min, max) {
+    let newMin = isNumber(min) ? min : 0;
+    let newMax = isNumber(max) ? max : min;
+
+    if (newMin > newMax) {
+        [newMin, newMax] = [newMax, newMin];
+    }
+
+    return Math.floor(Math.random() * (newMax - newMin) + newMin);
+}
+
+module.exports = {
+    round,
+    numberFormat,
+    randomNumber,
+};
